c2f6b636fefe7bd66950761ab8aeb3056f4eae1e
WamiaRafa <wamiarafa@gmail.com>
2026-08-14 18:25:26 +0600

    Grade Checking with IF ELif

--- Control_Flow.py
+++ Control_Flow.py
@@ -4,4 +4,19 @@ if number % 2 == 0:
 else :
    print('Odd number')
 
-    
+#IF-ELSE
+marks=78
+if marks >= 90 :
+   grade="A+"
+elif marks >=80 :
+   grade ="A"      
+elif marks >=70 :
+   grade ="B"   
+elif marks >=60 :
+   grade ="C"   
+elif marks >=50 :
+   grade ="D"   
+else :
+   grade='F'
+print(marks, {grade})     
+      
