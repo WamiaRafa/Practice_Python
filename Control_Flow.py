08229af27c1ef93c264cc50795f221ded133efb7
WamiaRafa <wamiarafa@gmail.com>
2026-08-14 20:30:08 +0600

    loop , condition

--- Control_Flow.py
+++ Control_Flow.py
@@ -19,4 +19,36 @@ elif marks >=50 :
 else :
    grade='F'
 print(marks, {grade})     
-      
+
+#for loop 
+for i in range(5):
+   print(i)      
+fruits=[ "mango", "papaya", "banana"] 
+print(fruits) 
+
+#while loop
+count =1 
+
+while count<=5 :
+   print(count)
+   count+=1
+print("count>5")
+for i in range(1,11):
+   if i==6:
+    break 
+   print(i)
+for i in range(1,11):
+   if i==6:
+    continue 
+   print(i)   
+
+for i in range(1,51 ) :
+  if i % 3 == 0 and i % 5 == 0:
+     print("Fizz Buzz")
+
+  elif i % 3 == 0:
+     print("Fizz")
+  elif i % 5 == 0:
+     print("Buzz")  
+  else :
+     print(i)   
