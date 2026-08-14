368f0b1d283a849f133e9f8de9cafebe2a7855e1
WamiaRafa <wamiarafa@gmail.com>
2026-08-14 16:17:47 +0600

    Type checking

--- Python_intro.py
+++ Python_intro.py
@@ -2,5 +2,13 @@
 num1= 10
 num2= 20
 print(num1+num2)
-
 print(num1-num2)
+
+#type checking 
+name="rafa" #String 
+age=23 #integer
+salary=65000.575 #float
+isStudent=True #boolean
+
+print(type(name))
+print(type(age))
